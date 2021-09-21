0ed918fde60948c3e6fddd1072143eb3a8968a08
Scott Ford <swford@ualberta.ca>
2021-09-21 11:04:40 -0600

    added a comma

--- local_library/settings.py
+++ local_library/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # myapps
-    'catalog.apps.CatalogConfig'
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
