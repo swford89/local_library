f1b5a6389ea2f076ba87ca1b07aaded5ff370f9f
Scott Ford <swford@ualberta.ca>
2021-10-08 17:16:21 -0600

    adding tests for: models, forms, views

---
+++ catalog/tests/test_forms.py
@@ -0,0 +1,26 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from catalog.forms import RenewBookForm
+
+class RenewBookFormTest(TestCase):
+
+    def test_renew_form_date_field_label(self):
+        pass
+
+    def test_renew_form_date_field_help_text(self):
+        pass
+
+    def test_renew_form_date_in_past(self):
+        pass
+
+    def test_renew_form_date_too_far_in_future(self):
+        pass
+
+    def test_renew_form_date_today(self):
+        pass
+
+    def test_renew_form_date_max(self):
+        pass
