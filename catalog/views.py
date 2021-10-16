c00ea7c95f06aac7c8e239309f2d0b9528d8a6ea
Scott Ford <swford@ualberta.ca>
2021-10-16 10:58:05 -0600

    fixed indentation error with context dictionary in renew_book_librarian view

--- catalog/views.py
+++ catalog/views.py
@@ -138,9 +138,9 @@ def renew_book_librarian(request, pk):
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
         form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
 
-        context = {
-            "form": form,
-            "book_instance": book_instance,
-        }
+    context = {
+        "form": form,
+        "book_instance": book_instance,
+    }
 
     return render(request, "catalog/book_renew_librarian.html", context)
