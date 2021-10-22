f1ef4911ae5c40db2750484e79fc1887fa979d1e
Scott Ford <swford@ualberta.ca>
2021-10-22 10:45:02 -0600

    figured out Python.h issue and got django-heroku installed; imported, then added to bottom of script

--- local_library/settings.py
+++ local_library/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
